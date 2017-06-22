3fe8564624d77c7584f83e972733a0effb042e1f
Shawn <azaaza987@gmail.com>
2017-06-22 15:51:22 +0800

    junit case Demo update

--- src/test/java/com/aig/postsaleadmin/TestUserService.java
+++ src/test/java/com/aig/postsaleadmin/TestUserService.java
@@ -9,6 +9,8 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -23,7 +25,7 @@ import com.web.soulyogaadmin.employee.service.IEmployeeAccountService;
  */
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath:spring.xml"})
+@ContextConfiguration(locations={"classpath:spring.xml","classpath:spring-hibernate.xml"})
 public class TestUserService {
 
 //	IUserService userService = new UserServiceImpl();
@@ -41,11 +43,11 @@ public class TestUserService {
 	public static void tearDownAfterClass() throws Exception {
 	}
 
-	@Before
-	public void setUp() throws Exception {
+//	@Before
+//	public void setUp() throws Exception {
 //		ApplicationContext ac = new ClassPathXmlApplicationContext(new String[] { "classpath:spring.xml", "classpath:spring-hibernate.xml" });
 //		employeeAccountService = (IEmployeeAccountService) ac.getBean("employeeAccountService");
-	} 
+//	} 
 
 	@After
 	public void tearDown() throws Exception {
